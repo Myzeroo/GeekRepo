419878fb5fd83af8f32a41bc3aa18b770f4c7f95
ArtemL <plazzza@mail.ru>
2020-10-09 10:43:10 +0300

    HW7.1

---
+++ HomeWork_07/Cat.java
@@ -0,0 +1,15 @@
+package HomeWork_07;
+
+public class Cat {
+    private String name;
+    private int appetite;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat(Plate p) {
+        p.decreaseFood(appetite);
+    }
+}
