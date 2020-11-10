fdee214351aa11101e1f32c2f7d8987f7f3b4910
ArtemL <plazzza@mail.ru>
2020-11-10 21:39:18 +0300

    HW.2.6

---
+++ HomeWork_2_06/MyClient.java
@@ -0,0 +1,63 @@
+package HomeWork_2_06;
+
+import HomeWork_2_04.SortTreeMap.MyComparator;
+
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MyClient {
+    public static void main(String[] args) {
+        new MyClient();
+    }
+
+    private Socket clientSocket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public MyClient() {
+        try {
+            clientSocket = new Socket("127.0.0.1", 8899);
+            System.out.println("Соединение с сервером устанвлено.");
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Thread outThread = new Thread(() -> {
+            try {
+                while (true) {
+                    String outgoingMessage = new Scanner(System.in).nextLine();
+                    out.writeUTF(outgoingMessage);
+                    if (outgoingMessage.equals("--exit")) break;
+                    System.out.println("Client: " + outgoingMessage);
+                }
+                in.close();
+                out.close();
+                clientSocket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        outThread.setDaemon(true);
+        outThread.start();
+
+        try {
+            while (true) {
+                String incomingMessage = in.readUTF();
+                if (incomingMessage.equals("--exit")) {
+                    System.out.println("соединение превано со стороны сервера");
+                    break;
+                }
+                System.out.println("Server: " + incomingMessage);
+            }
+        } catch (IOException e) {
+            System.out.println("соединение с сервером прервано");
+        }
+    }
+}
---
+++ HomeWork_2_06/MyServer.java
@@ -0,0 +1,63 @@
+package HomeWork_2_06;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class MyServer {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+
+    private ServerSocket srvSocket;
+    private Socket clntSocket;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    public MyServer() {
+        try {
+            srvSocket = new ServerSocket(8899);
+            System.out.println("Сервер ожидает соединения с клиентом.");
+            clntSocket = srvSocket.accept();
+            System.out.println("Соединение с клиентом установлено.");
+            out = new DataOutputStream(clntSocket.getOutputStream());
+            in = new DataInputStream(clntSocket.getInputStream());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Thread outThread = new Thread(() -> {
+            try {
+                while (true) {
+                    String outgoingMessage = new Scanner(System.in).nextLine();
+                    out.writeUTF(outgoingMessage);
+                    if (outgoingMessage.equals("--exit")) break;
+                    System.out.println("Server: " + outgoingMessage);
+                }
+            } catch (IOException e) {
+                System.out.println("outing");
+            }
+        });
+        outThread.setDaemon(true);
+        outThread.start();
+
+        try {
+            while (true) {
+                String incomingMessage = in.readUTF();
+                if (incomingMessage.equals("--exit")) {
+                    System.out.println("соединение превано со стороны клиента");
+                    break;
+                }
+                System.out.println("Client: " + incomingMessage);
+            }
+        } catch (IOException e) {
+            System.out.println("соединение с клиентом прервано");
+        }
+    }
+}
