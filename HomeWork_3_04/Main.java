c41ab53ece9f2eb685be2f9f7cf27efac75debac
ArtemL <plazzza@mail.ru>
2020-12-07 17:14:27 +0300

    HW.3.4.1

--- HomeWork_3_04/Main.java
+++ HomeWork_3_04/Main.java
@@ -1,66 +1,72 @@
 package HomeWork_3_04;
 
 public class Main {
-    private final Object monitor = new Object();
-    private volatile char currentLetter = 'A';
+//    private final Object monitor = new Object();
+    private static volatile char currentLetter = 'A';
 
     public static void main(String[] args) {
-        Main w = new Main();
+        Main w1 = new Main();
 
-        new Thread(() -> {w.printA();}).start();
-        new Thread(() -> {w.printB();}).start();
-        new Thread(() -> {w.printC();}).start();
+        new Thread(() -> {
+            w1.printA();
+        }).start();
+        new Thread(() -> {
+            w1.printB();
+        }).start();
+        new Thread(() -> {
+            w1.printC();
+        }).start();
     }
 
-    public void printA() {
-        synchronized (monitor) {
+    public synchronized void printA() {
+     //   synchronized (monitor) {
             try {
                 for (int i = 0; i < 5; i++) {
                     while (currentLetter != 'A') {
-                        monitor.wait();
+                        wait();
                     }
-                    monitor.notifyAll();
+                    notifyAll();
                     System.out.print("A");
                     currentLetter = 'B';
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+     //   }
     }
 
-    public void printB() {
-        synchronized (monitor) {
+    public synchronized void printB() {
+     //   synchronized (monitor) {
             try {
                 for (int i = 0; i < 5; i++) {
                     while (currentLetter != 'B') {
-                        monitor.wait();
+                        wait();
                     }
-                    monitor.notifyAll();
+                    notifyAll();
                     System.out.print("B");
                     currentLetter = 'C';
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+      //  }
     }
 
-    public void printC() {
-        synchronized (monitor) {
+    public synchronized void printC() {
+     //   synchronized (monitor) {
             try {
                 for (int i = 0; i < 5; i++) {
                     while (currentLetter != 'C') {
-                        monitor.wait();
+                        wait();
                     }
-                    monitor.notifyAll();
+                    notifyAll();
                     System.out.print("C");
                     currentLetter = 'A';
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
+     //   }
     }
 }
 
